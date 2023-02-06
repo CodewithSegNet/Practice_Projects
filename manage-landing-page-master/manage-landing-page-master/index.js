72fb6b49a3d3f05fdc366825bb8015c808e75841
Olusegun Emmanuel <91101596+CodewithSegNet@users.noreply.github.com>
2023-02-06 02:48:39 +0100

    added new file

--- manage-landing-page-master/manage-landing-page-master/index.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector('.btn-hamburger');
-const nav = document.querySelector('nav');
-
-
-burger.addEventListener('click', function() {
-    nav.classList.toggle('showlinks');
-
-});
