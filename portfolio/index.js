70ccb93706c1615e90e6136cb9e8d4e158da267d
segunNew <eeomo@yahoo.com>
2023-06-15 10:41:29 -0700

    portfolio for alx

---
+++ portfolio/index.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger');
+
+
+//adding eventListener to burger to access btn 
+burger.addEventListener('click', function() {
+ 
+        });
+       
+    }else{
+        
+
+         //adding forEach statement to animation 
+ 
+    }
+});
